13cd79e357e665d90bebad47a82eea6cccb98d9a
Dennis Ferron <system.windows.codeslinger@gmail.com>
2022-01-31 21:26:06 -0600

    Remove app studio specific lines and comments from resource file because we're defining resources manually not using app studio.

--- resource.h
+++ resource.h
@@ -8,14 +8,3 @@
 #define IDC_SNAKES                       1004
 #define IDC_TBD                      1005
 //#define IDC_SHOWCOUNT                   1006
-
-// Next default values for new objects
-//
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1007
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
